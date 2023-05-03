5819af9ecd167aecb4f39fde74e87bcdcb2093f4
Mayur-prajapati9094 <mayurprajapati9094@gmail.com>
2023-05-03 17:51:23 +0530

    display username

--- post.js
+++ post.js
@@ -3,14 +3,19 @@ let imageArr = [];
 getData();
 displayImages();
 
+let uname = JSON.parse(localStorage.getItem("uname"));
 input.addEventListener("change", (event) => {
   const files = event.target.files;
   for (var i = 0, f; (f = files[i]); i++) {
     let convertMB = (files[i].size / (1024 * 1024)).toFixed(2);
     var reader = new FileReader();
+
     reader.onload = function (e) {
+      var testObj = { uname: uname[0].uname, imageUrl: e.target.result };
+
       if (convertMB <= 2) {
-        imageArr.push(e.target.result);
+        imageArr.push(testObj);
+
         displayImages();
         setData();
       }
@@ -22,13 +27,12 @@ input.addEventListener("change", (event) => {
 function displayImages() {
   let images = "";
   let likeCount = 0;
-  let uname = JSON.parse(localStorage.getItem("uname"))
-  console.log(uname)
+
   imageArr.forEach((image, index) => {
-    console.log(uname[0].uname)
+    // console.log(userData[0].uname)
     images += `<div class="container">
-        
-        <img class="image" src="${image}">
+        <span>${image.uname}</span>
+        <img class="image" src="${image.imageUrl}">
         <span class="delete" onclick="deleteImages(${index})">&times;</span>
         <button class=emoji>&#x1F44D;</button> 
         <span>${likeCount}</span>
@@ -52,4 +56,3 @@ function deleteImages(index) {
   setData();
   displayImages();
 }
-
